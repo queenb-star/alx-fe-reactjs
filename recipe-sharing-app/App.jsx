79262985ba1fb61e799c126e78166a19d5cab8aa
queenb-star <benishimwe011@gmail.com>
2025-11-13 07:04:20 -0800

    Update recipeStore.js with add, update, delete actions

--- recipe-sharing-app/App.jsx
+++ recipe-sharing-app/App.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
-import './App.css';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipes/:id" element={<RecipeDetailsWrapper />} />
+      </Routes>
+    </Router>
   );
 }
+
+// Wrapper to get ID from URL params
+import { useParams } from 'react-router-dom';
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams();
+  return <RecipeDetails recipeId={parseInt(id)} />;
+};
 
 export default App;
 
---
+++ recipe-sharing-app/RecipeList.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from './recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <Link to={`/recipes/${recipe.id}`}>
+            <h3>{recipe.title}</h3>
+          </Link>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
+
+
