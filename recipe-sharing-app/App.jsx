5721656cf5077024a8b46c6175c74a2255b82150
queenb-star <benishimwe011@gmail.com>
2025-11-13 03:47:42 -0800

    Add correct imports for AddRecipeForm and RecipeList in App.jsx

---
+++ recipe-sharing-app/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
+
